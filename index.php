88c4ea36c5dec12454c1924303536a987c56c88b
dhustlerz <jashan.pahwa007@gmail.com>
2015-08-02 15:44:11 -0400

    change redirection

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("http://dhustlerz.com/dhustlerz/public/");
+header("refresh: 0; http://dhustlerz.com/dhustlerz/public/");
 
 	echo '<title>Laravel Installed</title><div style="background: #e9ffed; border: 1px solid #b0dab7; padding: 15px;" align="center" >
 	<font size="5" color="#182e7a">Laravel is installed successfully.</font><br /><br />
