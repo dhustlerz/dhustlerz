48a16234929160175bb14c641b568e033b03166c
dhustlerz <jashan.pahwa007@gmail.com>
2015-08-08 02:05:58 -0400

    layout update

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>D-Hustlerz</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    @yield('stylesheets')
+  </head>
+  <body>
+
+    <div class="container-fluid">
+
+      <div class="row">
+        @yield('content')
+      </div>
+
+    </div>
+
+
+
+
+
+
+  <!-- Latest compiled and minified JavaScript jQuery-->
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <!-- Latest compiled and minified JavaScript jQuery UI-->
+  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+  <!-- Latest compiled and minified JavaScript Bootstrap -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+  @yield('JavaScripts')
+  </body>
+</html>
+
+
+
